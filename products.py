03b83284bd487fdabd83096789540e23d02801ac
cwleungaa <cwleungaa@gmail.com>
2019-03-04 02:36:02 +0800

    Version 2

--- products.py
+++ products.py
@@ -3,11 +3,12 @@ while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
 		break
-	price = input('請輸入商品價格: ')
-	p = []
-	p.append(name)
-	p.append(price)
-	products.append(p)	
+	price = input('請輸入商品價格: ') 
+	products.append([name,price])	
 print(products)
 
-print(products[1][0])
+for p in products:
+	print(p[0] + ' 的價格是 ' + p[1])
+
+
+
